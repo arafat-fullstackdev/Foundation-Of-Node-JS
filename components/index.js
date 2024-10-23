220891bf8577f69cc0fbfc6776f46f5af07fb7fc
Arafat Patwary <farabipsk@gmail.com>
2024-10-23 17:17:33 +0600

    Initial commit

---
+++ components/index.js
@@ -0,0 +1,5 @@
+const path = require('path');
+
+const myPath = 'D:\Node_JS\Foundation\components\index.js';
+
+console.log(path.basename(myPath));
---
+++ components/script1.js
@@ -0,0 +1,10 @@
+var people ={
+    name: 'Sergi Lavrob',
+
+}
+// const test => fun(){
+//  console.log('fun run!');
+
+// }
+
+module.export = people;
