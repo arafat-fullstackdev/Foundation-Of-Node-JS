2d0cdfbab09ea87f3518d8242674d6f9b04eec52
Arafat Patwary <farabipsk@gmail.com>
2024-10-23 18:06:34 +0600

    use path code

--- components/index.js
+++ components/index.js
@@ -2,4 +2,7 @@ const path = require('path');
 
 const myPath = 'D:\Node_JS\Foundation\components\index.js';
 
-console.log(path.basename(myPath));
+// console.log(path.basename(myPath));
+// console.log(path.dirname(myPath));
+// console.log(path.extname(myPath));
+console.log(path.parse(myPath));
