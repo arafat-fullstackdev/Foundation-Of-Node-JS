e1bbad81695fbb296b5b3d014bc7a6b2e5efbe80
Arafat Patwary <farabipsk@gmail.com>
2024-10-23 18:21:43 +0600

    includeing fs and os

--- components/index.js
+++ components/index.js
@@ -1,8 +1,13 @@
-const path = require('path');
+// const path = require('path');
 
-const myPath = 'D:\Node_JS\Foundation\components\index.js';
+// const myPath = 'D:\Node_JS\Foundation\components\index.js';
 
 // console.log(path.basename(myPath));
 // console.log(path.dirname(myPath));
 // console.log(path.extname(myPath));
-console.log(path.parse(myPath));
+// console.log(path.parse(myPath));
+// const os = require('os');
+// console.log(os.cpus());
+const fs = require('fs');
+fs.writeFileSync('myfile.txt', 'Hello Programmers');
+fs.appendFileSync('myfile.txt', ' How are you?');
