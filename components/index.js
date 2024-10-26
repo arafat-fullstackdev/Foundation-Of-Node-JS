dd3e30000b47330f6b3277d74311c3e6ba6e5b0f
Arafat Patwary <farabipsk@gmail.com>
2024-10-26 12:23:32 +0600

    Add fs file

--- components/index.js
+++ components/index.js
@@ -7,10 +7,17 @@
 // console.log(path.extname(myPath));
 // console.log(path.parse(myPath));
 // const os = require('os');
-// console.log(os.cpus());
-const fs = require('fs');
+// console.log(os.cpus());const fs = require('fs');//
 // fs.writeFileSync('myfile.txt', 'Hello Programmers');
 // fs.appendFileSync('myfile.txt', ' How are you?');
 
-const data = fs.readFileSync('myfile.txt');
-console.log(data.toString());
+// const data = fs.readFileSync('myfile.txt');
+// console.log(data.toString());
+
+// const os = require('os');
+// console.log(os.freemem()); 
+const fs = require('fs');
+ fs.readFile('myfile.txt', (err, data) =>{
+    console.log(data.toString());
+});
+console.log("Hello Coder!");
