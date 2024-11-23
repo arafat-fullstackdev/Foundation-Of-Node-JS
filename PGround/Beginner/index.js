79f27d24abb6e00866975fb51b0cc2044937e33e
Arafat Patwary <farabipsk@gmail.com>
2024-11-23 09:48:49 +0600

    Add Object

--- PGround/Beginner/index.js
+++ PGround/Beginner/index.js
@@ -3,12 +3,15 @@ const EventEmitter = require("events");
 const emitter = new EventEmitter();
 
 // register listener for bellring event
-emitter.on("bellRing", (period) => {
-  console.log(`We need to go! ${period}`);
+emitter.on("bellRing", (period, text) => {
+  console.log(`We need to go! ${period} ${text}`);
 });
 
 setTimeout(() => {
-  emitter.emit("bellRing", "First");
+  emitter.emit("bellRing", {
+    period: "First Period",
+    text: "From Node Js",
+  });
 }, 2000);
 
 setTimeout(() => {
